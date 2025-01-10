de066d72c0b5910a1303c7450da23fb7ebb20332
Alberto Arracó <aarraco@k-tuin.com>
2025-01-10 22:17:14 +0100

    Cambios pequeños

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -3,7 +3,7 @@ import {persistor} from "../../store/configureStore";
 
 const Header = () => {
 	return (<footer>
-		<p>Si Rick lo leyera, probablemente destruiría este footer en un microsegundo. Morty dice que está bien, pero, ¿a quién le importa lo que diga Morty?</p>
+		<p>“Sometimes science is more art than science, Morty. A lot of people don’t get that.” – Rick Sanchez</p>
 		<p><small>© 2025, Rick y Morty Fan Page</small></p>
 	</footer>)
 }
