85236251ca6c87edc6b0e3220dfcf79e00ad6150
Alberto Arracó <aarraco@k-tuin.com>
2025-01-10 22:52:21 +0100

    Fix loadings

--- src/components/placeholder.js
+++ src/components/placeholder.js
@@ -7,9 +7,8 @@ const PlaceholderItemList = () => {
 	const type = id ? 'detail' : 'list',
 		loaders = {
 			list: (
-				<lottie-player src="https://lottie.host/32088d8f-efaf-4463-8681-6f0e8ac3a783/SRpeTrRQjE.json"
-				               background="##FFFFFF"
-				               speed="1" style={{width: '50%', height: 'auto', margin: 'auto'}} loop autoplay direction="1"
+				<lottie-player src="https://lottie.host/28e465df-8743-4934-8694-0a1b0d37b6d6/AmuIpbm5iw.json"
+				               speed="1" loop autoplay direction="1"
 				               mode="normal"></lottie-player>
 			),
 			detail: (
@@ -20,7 +19,7 @@ const PlaceholderItemList = () => {
 		};
 
 	return (
-		<div className={'loading-placeholder'}>
+		<div className={`loading-placeholder loading-${type}`}>
 			{loaders[type]}
 			<h3>Loading {type}....</h3>
 		</div>
